742a635eb070160f6e9b6cb112ce1b2d71025d0b
Hrishikesh Dehankar <c8hrish.dehankar@gmail.com>
2020-06-04 23:27:36 +0530

    Custom component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>I'm an Albatraoz..!</h1>
-        <h2>Vu-Vu..VU-VU-VU</h2>
+        <p>Vu-Vu..VU-VU-VU</p>
+        <Person/>
       </div>
     );
   }
