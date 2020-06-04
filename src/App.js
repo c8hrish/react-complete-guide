c8a4a073e0e844e25bb22a4f537bc736db92eede
Hrishikesh Dehankar <c8hrish.dehankar@gmail.com>
2020-06-04 16:31:49 +0530

    template div changed customized

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>I'm an Albatraoz!</h1>
+        <h1>I'm an Albatraoz..!</h1>
       </div>
     );
   }
