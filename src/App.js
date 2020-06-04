9750e772a34368453fa2ab41f9a04c7faf2c4250
Hrishikesh Dehankar <c8hrish.dehankar@gmail.com>
2020-06-05 00:31:52 +0530

    state commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    deejays: [
+      { name:'Ganu', exp:'4'},
+      { name:'Bob', exp:'2'},
+      { name:'Subbu', exp:'3'}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>I'm an Albatraoz..!</h1>
         <p>Vu-Vu..VU-VU-VU</p>
-        <Person name='Ganu' exp='4'/>
-        <Person name='Bob' exp='2'>My hobby is to sing loudly and badly</Person>
-        <Person name='Subbu' exp='3'/>
+        <button>Switch Name</button>
+        <Person name={this.state.deejays[0].name} exp={this.state.deejays[0].exp}/>
+        <Person name={this.state.deejays[1].name} exp={this.state.deejays[1].exp}>My hobby is to sing loudly and badly</Person>
+        <Person name={this.state.deejays[2].name} exp={this.state.deejays[2].exp}/>
       </div>
     );
   }
